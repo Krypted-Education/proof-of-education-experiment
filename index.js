48697818560d32ce272b7cc240ff42f0d4aca022
msusur <mail@mertsusur.com>
2018-04-11 00:33:18 +0100

    first stage is working

--- index.js
+++ index.js
@@ -1,64 +1,73 @@
-var web3 = require("web3");
-web3 = new web3(new web3.providers.HttpProvider("http://localhost:8545"));
-var OracleTest= new web3.eth.contract([
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "code",
-				"type": "string"
-			}
-		],
-		"name": "AddMessage",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [
-			{
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "addresses",
-		"outputs": [
-			{
-				"name": "",
-				"type": "address"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [
-			{
-				"name": "fromWho",
-				"type": "address"
-			}
-		],
-		"name": "PopMessage",
-		"outputs": [
-			{
-				"name": "code",
-				"type": "string"
-			},
-			{
-				"name": "isRead",
-				"type": "bool"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	}
-], 0xa11f649c055e813117a92010a4d4037a23a34640 );
-OracleTest.methods.PopMessage.call()
-.then(console.log);
-
-
+var Web3 = require('web3');
+web3 = new Web3(
+  new Web3.providers.HttpProvider(
+    'https://rinkeby.infura.io/yRg7y1dPJVCZWA6A0fhH'
+  )
+);
+var OracleTest = new web3.eth.Contract(
+  [
+    {
+      constant: false,
+      inputs: [
+        {
+          name: 'code',
+          type: 'string'
+        }
+      ],
+      name: 'AddMessage',
+      outputs: [],
+      payable: false,
+      stateMutability: 'nonpayable',
+      type: 'function'
+    },
+    {
+      constant: true,
+      inputs: [
+        {
+          name: '',
+          type: 'uint256'
+        }
+      ],
+      name: 'addresses',
+      outputs: [
+        {
+          name: '',
+          type: 'address'
+        }
+      ],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function'
+    },
+    {
+      constant: true,
+      inputs: [
+        {
+          name: 'fromWho',
+          type: 'address'
+        }
+      ],
+      name: 'PopMessage',
+      outputs: [
+        {
+          name: 'code',
+          type: 'string'
+        },
+        {
+          name: 'isRead',
+          type: 'bool'
+        }
+      ],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function'
+    }
+  ],
+  '0x27526290ab272fb2130a61e399bd9dd6515ac75a'
+);
+OracleTest.methods
+  .addresses(0)
+  .call()
+  .then((address) => {
+    console.log(`There is an address: ${address}`);
+  });
